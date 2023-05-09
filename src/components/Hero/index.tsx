da6c0ae26844e4e49a07cbd2094fc56fc1804057
Yonatan Tesfaye <yonatantesfaye@pop-os.localdomain>
2023-05-09 14:12:46 +0300

    add animation in hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { images } from "../../assets";
-import hero from "../../assets/hero.svg"
+import hero from "../../assets/hero.svg";
+import { motion } from "framer-motion";
+import { animation } from "../../Animations/index";
+import { useInView } from "react-intersection-observer";
 
 const Hero = () => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    rootMargin: "100px",
+  });
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  if (inView && !hasAnimated) {
+    setHasAnimated(true);
+  }
+
   return (
-    <div className="h-screen py-4 px-6 lg:flex ">
-      <div className="lg:flex lg:flex-row-reverse items-center justify-center bg-primary/5">
-        <div className="lg:w-[50%]">
-          <img src={hero} alt="" className="w-30"/>
-        </div>
-        <div className="flex flex-col items-center justify-center text-center lg:w-[50%]">
+    <div className="h-screen py-4 px-6 lg:flex">
+      <div className="lg:flex lg:flex-row-reverse items-center justify-center bg-primary/5 w-full">
+        <motion.div
+          variants={animation.aboutTextAnimation}
+          ref={ref}
+          animate={hasAnimated ? "visible" : "hidden"}
+          transition={{
+            delay: 0.02,
+            type: "tween",
+            duration: 1,
+          }}
+          className="lg:w-[50%]"
+        >
+          <img src={hero} alt="" className="w-30" />
+        </motion.div>
+        <motion.div
+          variants={animation.aboutImage}
+          ref={ref}
+          animate={hasAnimated ? "visible" : "hidden"}
+          transition={{
+            delay: 0.02,
+            type: "tween",
+            duration: 1,
+          }}
+          className="flex flex-col items-center justify-center text-center lg:w-[50%]"
+        >
           <h1 className="text-5xl lg:text-7xl font-bold my-3">
-          Unleash Your Potential with <span className="text-primary">HORN.</span>
+            Unleash Your Potential with{" "}
+            <span className="text-primary">HORN.</span>
           </h1>
           <p className="text-center lg:text-xl">
-          Elevate Your Growth. Drive Impact. Partner with HIRCTF for expert research, consultancy, and training services. Unlock new possibilities and accelerate your success today.
+            Elevate Your Growth. Drive Impact. Partner with HIRCTF for expert
+            research, consultancy, and training services. Unlock new
+            possibilities and accelerate your success today.
           </p>
-          <div className="bg-primary text-white px-5 py-3 font-bold my-3" >
+          <div className="bg-primary text-white px-5 py-3 font-bold my-3">
             Read More
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
